e366c3c5ecb3501a873208f8fd778b7093eeb409
Sergey Klyukin <saklyuki@yandex.ru>
2023-06-29 01:43:52 +0300

    пр14 на тесты 10

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,7 +18,7 @@ const deleteCard = (req, res, next) => {
       if (!card) {
         throw new NotFoundError('Карточка не найдена');
       }
-      if (card.owner === req.user._id) {
+      if (card.owner.toString() === req.user._id) {
         Card.findByIdAndRemove(req.params.cardId);
       } else {
         throw new ForbiddenError('Недостаточно прав');
